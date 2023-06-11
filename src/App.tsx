cfa933f51922c730644a8cb2a5772e81f61ea366
Hiroaki Nagasaka <hiroaki.nagasaka37@gmail.com>
2023-06-11 11:16:26 +0900

    gts追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useRef, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { ReactComponent as Test } from "./test.drawio.svg";
-import chroma from "chroma-js";
+import React, {useEffect, useRef, useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {ReactComponent as Test} from './test.drawio.svg';
+import chroma from 'chroma-js';
 
 function addEventToRectangle(
   ref: React.RefObject<SVGSVGElement>,
   name: string,
-  click: ()=>void
+  click: () => void
 ) {
   if (ref.current) {
     const nodes = Array.from(
@@ -17,27 +17,27 @@ function addEventToRectangle(
       const children = Array.from(node.children);
       const originalColors = new Array<string | null>();
       for (const child of children) {
-        originalColors.push(child.getAttribute("fill"));
+        originalColors.push(child.getAttribute('fill'));
       }
-      node.addEventListener("click", click);
-      node.addEventListener("mouseover", () => {
-        console.log("mouseover");
+      node.addEventListener('click', click);
+      node.addEventListener('mouseover', () => {
+        console.log('mouseover');
         for (let i = 0; i < node.children.length; i++) {
           const child = node.children[i];
-          const color = originalColors[i]
-          if (child.tagName === "rect" && color) {
-            const newColor = chroma(color).darken().hex()
-            child.setAttribute("fill", newColor);
+          const color = originalColors[i];
+          if (child.tagName === 'rect' && color) {
+            const newColor = chroma(color).darken().hex();
+            child.setAttribute('fill', newColor);
           }
         }
       });
-      node.addEventListener("mouseout", () => {
-        console.log("mouseout");
+      node.addEventListener('mouseout', () => {
+        console.log('mouseout');
         for (let i = 0; i < node.children.length; i++) {
           const child = node.children[i];
-          if (child.tagName === "rect") {
+          if (child.tagName === 'rect') {
             const newColor = originalColors[i];
-            child.setAttribute("fill", newColor ? newColor : "#000000");
+            child.setAttribute('fill', newColor ? newColor : '#000000');
           }
         }
       });
@@ -48,10 +48,10 @@ function addEventToRectangle(
 function App() {
   const ref = useRef<SVGSVGElement>(null);
   useEffect(() => {
-    console.log("add event listener");
-    addEventToRectangle(ref, "rect1", ()=>{});
-    addEventToRectangle(ref, "rect2", ()=>{});
-    addEventToRectangle(ref, "rect3", ()=>{});
+    console.log('add event listener');
+    addEventToRectangle(ref, 'rect1', () => {});
+    addEventToRectangle(ref, 'rect2', () => {});
+    addEventToRectangle(ref, 'rect3', () => {});
   }, []);
 
   return (
