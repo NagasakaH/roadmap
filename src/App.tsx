4de938ae1bea0c46bd00ec2904729d21cefcd769
Hiroaki Nagasaka <hiroaki.nagasaka37@gmail.com>
2023-06-11 11:06:53 +0900

    ロードマップ追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { ReactComponent as Test } from "./test.drawio.svg";
+import chroma from "chroma-js";
+
+function addEventToRectangle(
+  ref: React.RefObject<SVGSVGElement>,
+  name: string,
+  click: ()=>void
+) {
+  if (ref.current) {
+    const nodes = Array.from(
+      ref.current.querySelectorAll('[data-name="' + name + '"]')
+    );
+    for (const node of nodes) {
+      const children = Array.from(node.children);
+      const originalColors = new Array<string | null>();
+      for (const child of children) {
+        originalColors.push(child.getAttribute("fill"));
+      }
+      node.addEventListener("click", click);
+      node.addEventListener("mouseover", () => {
+        console.log("mouseover");
+        for (let i = 0; i < node.children.length; i++) {
+          const child = node.children[i];
+          const color = originalColors[i]
+          if (child.tagName === "rect" && color) {
+            const newColor = chroma(color).darken().hex()
+            child.setAttribute("fill", newColor);
+          }
+        }
+      });
+      node.addEventListener("mouseout", () => {
+        console.log("mouseout");
+        for (let i = 0; i < node.children.length; i++) {
+          const child = node.children[i];
+          if (child.tagName === "rect") {
+            const newColor = originalColors[i];
+            child.setAttribute("fill", newColor ? newColor : "#000000");
+          }
+        }
+      });
+    }
+  }
+}
 
 function App() {
+  const ref = useRef<SVGSVGElement>(null);
+  useEffect(() => {
+    console.log("add event listener");
+    addEventToRectangle(ref, "rect1", ()=>{});
+    addEventToRectangle(ref, "rect2", ()=>{});
+    addEventToRectangle(ref, "rect3", ()=>{});
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="overlay"></div>
+      <Test ref={ref} />
     </div>
   );
 }
